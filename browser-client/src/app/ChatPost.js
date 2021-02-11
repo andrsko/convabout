cbc0c8a22303d4e0c6704a615721d075bc441412
Andrii Savchenko <andrsvchnko@gmail.com>
2021-02-11 19:38:40 +0200

    Add back to feed button

--- browser-client/src/app/ChatPost.js
+++ browser-client/src/app/ChatPost.js
@@ -1,12 +1,25 @@
 import { SinglePostPage } from "../features/posts/SinglePostPage";
 import { ChatWindow } from "../features/chat/ChatWindow";
+
+import back from "./back.svg";
+
 import styles from "./ChatPost.module.css";
 
 export const ChatPost = () => {
+  const backToFeed = () => {
+    window.history.back();
+  };
+
   return (
-    <div className={styles.chatPost}>
-      {SinglePostPage()}
-      {ChatWindow()}
+    <div className={styles.chatPostWrapper}>
+      <button className={styles.back} type="button" onClick={backToFeed}>
+        <img src={back} alt="back" />
+      </button>
+      <div className={styles.backBackground} onClick={backToFeed} />
+      <div className={styles.chatPost}>
+        {SinglePostPage()}
+        {ChatWindow()}
+      </div>
     </div>
   );
 };

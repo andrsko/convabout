032e8ee58e5e72b908568ceffad7d4695d1c865d
Andrii Savchenko <andrsvchnko@gmail.com>
2021-02-16 15:24:08 +0200

    Fix tag links

--- browser-client/src/features/posts/Tag.js
+++ browser-client/src/features/posts/Tag.js
@@ -1,32 +1,27 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { activateTag, fetchPosts } from "./postsSlice";
 
 import styles from "./Tag.module.css";
 
 export const Tag = (props) => {
-  const history = useHistory();
   const dispatch = useDispatch();
 
   return (
-    <button
-      className={
-        styles.tag + (props.type === "trending" ? " " + styles.trending : "")
-      }
-      onClick={(e) => {
-        // prevent redirecting to chat
-        e.preventDefault();
-        e.stopPropagation();
-        e.nativeEvent.stopImmediatePropagation();
-
-        history.push(`/tag/${props.tag.name}`);
-        dispatch(activateTag(props.tag));
-        dispatch(fetchPosts(props.tag));
-      }}
-    >
-      {props.tag.name}
-    </button>
+    <Link to={`/tag/${props.tag.name}`}>
+      <button
+        className={
+          styles.tag + (props.type === "trending" ? " " + styles.trending : "")
+        }
+        onClick={(e) => {
+          dispatch(activateTag(props.tag));
+          dispatch(fetchPosts(props.tag));
+        }}
+      >
+        {props.tag.name}
+      </button>
+    </Link>
   );
 };
